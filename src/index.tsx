9d8e344f353fbd463491df3473a5b18df767422e
MohamadMehdiDabestani <mohamad1382mhd@gmail.com>
2021-05-20 17:14:46 +0430

    Testgoogle

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import App from "./Container/App";
 import { BrowserRouter } from "react-router-dom";
 import Load from "./Components/Load/Load";
-
 ReactDOM.render(
   <Suspense fallback={<Load />}>
     <BrowserRouter>
